824b8c5af525f7036bc96efc1a82b2805fea521f
swede1987 <40723088+swede1987@users.noreply.github.com>
2018-09-21 01:02:24 +0500

    Убрал все пробелы

--- rand.php
+++ rand.php
@@ -1,21 +1,15 @@
 <?php
-
 $chars = "qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP_\|/~!@#$%^&*,.";
 $lenght = 8;
-
 if (isset($_POST["submit"])) {
     $lenght = (int)$_POST["text"];
     $pass="";
     while($lenght--)
         $pass.=$chars[rand(0, strlen($chars)-1)];
         echo $pass;
-
 }
 ?>
-
 <form action="" method="post" name="count">
-
     <input type="text" required name="text">
     <input type="submit" name="submit" value="Генерировать пароль">
-
 </form>
